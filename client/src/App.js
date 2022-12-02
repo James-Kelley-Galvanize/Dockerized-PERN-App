3b5747723dc0452f9d8ee86cae147f075607ed38
James-Kelley-Galvanize <james.kelley@galvanize.com>
2022-12-02 15:38:57 -0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,20 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [pageContent, setPageContent] = useState(
-    []
-  );
+  const [pageContent, setPageContent] = useState([]);
   useEffect(() => {
     fetch("http://localhost:3003/test_route")
       .then((res) => res.json())
       .then((json) => setPageContent(json))
       .catch((err) => setPageContent(err));
-  },[]);
+  }, []);
   return (
     <div className="App">
-      <h1>{pageContent.map(item=><div>{JSON.stringify(item)}</div>)}</h1>
+      <h1>
+        {pageContent.map((item) => (
+          <div>{JSON.stringify(item)}</div>
+        ))}
+      </h1>
     </div>
   );
 }
